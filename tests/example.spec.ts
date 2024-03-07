ed301b52612128616205da4c82e93eb32c7067ea
leandromoren <leandro.moren@outlook.com>
2024-03-07 13:39:32 -0300

    agrego page object

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,19 +1,30 @@
 import { test, expect } from '@playwright/test';
 import { allure } from 'allure-playwright';
+import { PlaywrightDevPage } from '../pageObject/playwright-dev-page';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await allure.suite('Playwright Dev Pages');
 })
 
-test('has title', async ({ page }) => {
-  // allure.suite me permite asignarle un nombre al caso en la suite de allure
-  await allure.suite("allure suite name 'has title'");
-  await expect(page).toHaveTitle(/Playwright/);
+test('getting started should contain table of contents', async ({ page }) => {
+  const playwrightDev = new PlaywrightDevPage(page);
+  await playwrightDev.goto();
+  await playwrightDev.getStarted();
+  await expect(playwrightDev.tocList).toHaveText([
+    `How to install Playwright`,
+    `What's Installed`,
+    `How to run the example test`,
+    `How to open the HTML test report`,
+    `Write tests using web first assertions, page fixtures and locators`,
+    `Run single test, multiple tests, headed mode`,
+    `Generate tests with Codegen`,
+    `See a trace of your tests`
+  ]);
 });
 
-test('get started link', async ({ page }) => {
-  await allure.suite("allure suite name 'get started link'");
-  await page.getByRole('link', { name: 'Get started' }).click();
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
-
+test('should show Page Object Model article', async({ page }) => {
+  const playwrightDev = new PlaywrightDevPage(page);
+  await playwrightDev.goto();
+  await playwrightDev.pageObjectModel();
+  await expect(page.locator('article')).toContainText('Page Object Model is a common pattern');
+})

b1bd194f29973b2f7df230845fd9f109e4e9f16c
leandromoren <leandro.moren@outlook.com>
2024-03-07 10:16:59 -0300

    asigno nombre a los casos en la suite allure

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,22 +1,19 @@
 import { test, expect } from '@playwright/test';
 import { allure } from 'allure-playwright';
 
-test('has title', async ({ page }) => {
-  await allure.suite("allure suite name 'has title'");
+test.beforeEach(async ({ page }) => {
   await page.goto('https://playwright.dev/');
+})
 
-  // Expect a title "to contain" a substring.
+test('has title', async ({ page }) => {
+  // allure.suite me permite asignarle un nombre al caso en la suite de allure
+  await allure.suite("allure suite name 'has title'");
   await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
   await allure.suite("allure suite name 'get started link'");
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
 
